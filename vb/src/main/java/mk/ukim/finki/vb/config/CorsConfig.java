545befe03607b33c58254c4086345c8728c86cb7
Vasil <vsilt2002@gmail.com>
2024-06-22 20:52:09 +0200

    push2

--- vb/src/main/java/mk/ukim/finki/vb/config/CorsConfig.java
+++ vb/src/main/java/mk/ukim/finki/vb/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package mk.ukim.finki.vb.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,10 +10,9 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost","http://localhost:3000") // Adjust with your frontend URLs
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+        registry.addMapping("/api/**") // Adjust the mapping pattern as needed
+                .allowedOrigins("http://localhost", "http://localhost:3001", "http://localhost:3000", "http://localhost:30080")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
     }
 }
