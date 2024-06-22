42ef3a909978a6768fe34207e2331bf3693becc3
Vasil <vsilt2002@gmail.com>
2024-06-22 20:28:10 +0200

    push

--- emt/src/axios.js
+++ emt/src/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:9091/api' || 'http://backend:9091/api',
-})
+    baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:9091/api' || 'http://backend:9091/api',})
 
 export default instance;
