581807cac81734c7d167f9ec251eff1221eaf11f
Vasil <vsilt2002@gmail.com>
2024-06-23 02:22:13 +0200

    Fixing node ports

--- vb/src/main/java/mk/ukim/finki/vb/config/CorsConfig.java
+++ vb/src/main/java/mk/ukim/finki/vb/config/CorsConfig.java
@@ -1,18 +1,18 @@
-package mk.ukim.finki.vb.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class CorsConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**") // Adjust the mapping pattern as needed
-                .allowedOrigins("http://localhost", "http://localhost:3001", "http://localhost:3000", "http://localhost:30080")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*");
-    }
-}
+//package mk.ukim.finki.vb.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class CorsConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/api/**") // Adjust the mapping pattern as needed
+//                .allowedOrigins("http://localhost:80")
+//                .allowedMethods("GET", "POST", "PUT", "DELETE")
+//                .allowedHeaders("*");
+//    }
+//}
